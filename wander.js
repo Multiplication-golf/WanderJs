2c12013d09eaf82095d55ef9c93257183b1fc382
Jack tru <165608541+Multiplication-golf@users.noreply.github.com>
2025-03-04 12:36:40 -0500

    Update wander.js

--- wander.js
+++ wander.js
@@ -1,5 +1,5 @@
 export class wanderer {
-  constructor (x,y,wanderRadius,baseX,baseY,speed) {
+  constructor (x,y,wanderRadius,baseX,baseY,speed, {bound = true} = {}) {
     this.x = x
     this.y = y
     this.wanderRadius = wanderRadius
@@ -9,6 +9,8 @@ export class wanderer {
     this.targetX = x
     this.targetY = y
     this.angle = 0.1;
+    this.reachedTarget = false;
+    this.bound = bound
   }
   #newtarget() {
     this.angle = this.getRandomAngle();
@@ -18,8 +20,23 @@ export class wanderer {
     this.targetY += wanderRadius * Math.sin(this.angle)
   }
   #getRandomAngle() {
-    let min = 0;
+    let min = -Math.pi;
     let max = Math.pi;
     return (Math.random() * (max - min + 1) + min);
+  }
+  #move() {
+    this.x += speed * Math.cos(this.angle)
+    this.y += speed * Math.sin(this.angle)
+  }
+  #isAtTarget() {
+    this.reachedTarget = (Math.abs(this.x-this.targetX) < this.speed*2 && Math.abs(this.y-this.targetY) < this.speed*2)
+  }
+  think() {
+    this.isAtTarget();
+    if (!this.reachedTarget) {
+      this.move();
+    } else {
+      this.newtarget()
+    }
   }
 }
