701d48364071a1f0e47d5a2574a037f64a1a0efc
Jack tru <165608541+Multiplication-golf@users.noreply.github.com>
2025-03-04 12:08:13 -0500

    Update wander.js

--- wander.js
+++ wander.js
@@ -4,9 +4,22 @@ export class wanderer {
     this.y = y
     this.wanderRadius = wanderRadius
     this.baseX = baseX
-    this.baseX = baseX
+    this.baseY = baseY
     this.speed = speed
     this.targetX = x
     this.targetY = y
+    this.angle = 0.1;
+  }
+  #newtarget() {
+    this.angle = this.getRandomAngle();
+    this.targetX = this.baseX
+    this.targetY = this.baseY
+    this.targetX += wanderRadius * Math.cos(this.angle)
+    this.targetY += wanderRadius * Math.sin(this.angle)
+  }
+  #getRandomAngle() {
+    let min = 0;
+    let max = Math.pi;
+    return (Math.random() * (max - min + 1) + min);
   }
 }
