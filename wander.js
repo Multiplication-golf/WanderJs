7aac88dd640a8b4bae52d01e3ad70aafaa74a2f9
Jack tru <165608541+Multiplication-golf@users.noreply.github.com>
2025-03-04 12:00:49 -0500

    Create base of wander.js

---
+++ wander.js
@@ -0,0 +1,12 @@
+export class wanderer {
+  constructor (x,y,wanderRadius,baseX,baseY,speed) {
+    this.x = x
+    this.y = y
+    this.wanderRadius = wanderRadius
+    this.baseX = baseX
+    this.baseX = baseX
+    this.speed = speed
+    this.targetX = x
+    this.targetY = y
+  }
+}
