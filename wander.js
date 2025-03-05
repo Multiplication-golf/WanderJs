1cc0950112134bd3c9f02be6d03165ed4da9db67
Jack tru <165608541+Multiplication-golf@users.noreply.github.com>
2025-03-04 19:00:41 -0500

    Update wander.js

--- wander.js
+++ wander.js
@@ -1,42 +1,58 @@
-export class wanderer {
-  constructor (x,y,wanderRadius,baseX,baseY,speed, {bound = true} = {}) {
-    this.x = x
-    this.y = y
-    this.wanderRadius = wanderRadius
-    this.baseX = baseX
-    this.baseY = baseY
-    this.speed = speed
-    this.targetX = x
-    this.targetY = y
+class wanderer {
+  constructor(x, y, wanderRadius, baseX, baseY, speed, { bound = true, frameSpeed = 2 } = {}) {
+    this.x = x;
+    this.y = y;
+    this.wanderRadius = wanderRadius;
+    this.baseX = baseX;
+    this.baseY = baseY;
+    this.speed = speed;
+    this.targetX = x+speed*8;
+    this.targetY = y+speed*8;
     this.angle = 0.1;
     this.reachedTarget = false;
-    this.bound = bound
+    this.bound = bound;
+    this.frame = 0
+    this.frameSpeed = frameSpeed
   }
-  #newtarget() {
-    this.angle = this.getRandomAngle();
-    this.targetX = this.baseX
-    this.targetY = this.baseY
-    this.targetX += wanderRadius * Math.cos(this.angle)
-    this.targetY += wanderRadius * Math.sin(this.angle)
+  #newtarget() { 
+    this.angle = this.#getRandomAngle();
+    this.targetX = this.baseX;
+    this.targetY = this.baseY;
+    this.targetX += this.wanderRadius * Math.cos(this.angle);
+    this.targetY += this.wanderRadius * Math.sin(this.angle);
   }
   #getRandomAngle() {
-    let min = -Math.pi;
-    let max = Math.pi;
-    return (Math.random() * (max - min + 1) + min);
-  }
+    let min = -Math.PI;
+    let max = Math.PI;
+    return Math.random() * (max - min + 1) + min;
+  } 
   #move() {
-    this.x += speed * Math.cos(this.angle)
-    this.y += speed * Math.sin(this.angle)
+    this.x -= this.speed * Math.cos(this.angle);
+    this.y -= this.speed * Math.sin(this.angle);
   }
   #isAtTarget() {
-    this.reachedTarget = (Math.abs(this.x-this.targetX) < this.speed*2 && Math.abs(this.y-this.targetY) < this.speed*2)
+    this.reachedTarget =
+      Math.abs(this.x - this.targetX) < this.speed * 7 &&
+      Math.abs(this.y - this.targetY) < this.speed * 7;
+  }
+  #reAling() {
+    this.angle = Math.atan2(this.y - this.targetY ,this.x - this.targetX)
   }
   think() {
-    this.isAtTarget();
+    this.#isAtTarget();
     if (!this.reachedTarget) {
-      this.move();
+      this.#move();
     } else {
-      this.newtarget()
+      this.#newtarget();
+    }
+    this.frame += this.frameSpeed
+    if (this.frame % 4) {
+      this.#reAling();
     }
   }
+  returnXY() {
+    return { x: this.x, y: this.y };
+  }
 }
+
+loop();
