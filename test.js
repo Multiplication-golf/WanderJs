f705e3c5109570279a11fcbafebfa795e3fbea57
Jack tru <165608541+Multiplication-golf@users.noreply.github.com>
2025-03-04 19:01:22 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const canvas = document.createElement("canvas");
+const canvas = document.createElement("canvas"); 
 document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
@@ -6,19 +6,30 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let dot = { x: canvas.width / 2, y: canvas.height / 2, radius: 5 };
+var wanderdot = new wanderer(
+  canvas.width / 2,
+  canvas.height / 2,
+  100,
+  canvas.width / 2,
+  canvas.height / 2,
+  1
+);
 
 function drawDot() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.closePath();
+  wanderdot.think();
+  var x, y;
+  var { x, y } = wanderdot.returnXY();
+  dot.x = x;
+  dot.y = y;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
 }
 
 function loop() {
-    drawDot();
-    requestAnimationFrame(loop);
+  drawDot();
+  requestAnimationFrame(loop);
 }
-
-loop();
